3cd0326a5273ba38b991e68d5326604967a0d25d
SamuelNIttala <nittalasamuel7@gmail.com>
2021-06-11 05:25:39 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-	user: "postgres",
-	password: "12345",
-	database: "homejam",
-	host: "localhost",
-	port: "5432",
-});
-module.exports = pool
