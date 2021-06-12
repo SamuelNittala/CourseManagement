8c429e81c37a59cd64e7ad7979f3cbdfe6e79363
SamuelNittala <nittalasamuel7@gmail.com>
2021-06-13 04:50:34 +0530

    DB Improve

--- db/database.sql
+++ db/database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE homejam;
 
 CREATE TABLE account(
 	account_id SERIAL PRIMARY KEY,
-	name VARCHAR(255),
+	name VARCHAR(255) UNIQUE,
 	password VARCHAR(255),
 	isTeacher BOOLEAN
 );
@@ -12,4 +12,10 @@ CREATE TABLE course(
 	course_name VARCHAR(255),
 	instructor_name VARCHAR(255),	
 	account_id INT
+);
+
+CREATE TABLE enrolled(
+	enroll_id SERIAL PRIMARY KEY,
+	account_id INT,
+	course_id INT
 );
